b2ec55660531b8038c96f7cf77d78e47142cd11a
vawmpira <127033934+vawmpira@users.noreply.github.com>
2023-09-16 22:17:22 +0300

    Update esp.lua

--- esp.lua
+++ esp.lua
@@ -4,7 +4,7 @@ local function highlightPlayer(player)
 	local highlight = Instance.new("Highlight")
 	highlight.Parent = player.Character
 	highlight.FillColor = Color3.fromRGB(255,0,0)
-	highlight.OutlineColor = Color3.fromRGB(0, 0, 0)
+	highlight.OutlineColor = Color3.fromRGB(255, 255, 255)
 end
 
 for _, player in ipairs(Players:GetPlayers()) do
